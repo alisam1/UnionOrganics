27cda1dbdd6a8fdcdb383c46715110fc7ed3c5a8
alisam1 <alisa-m2@yandex.ru>
2019-09-11 13:29:05 +0300

    Update style

--- build/js/main.js
+++ build/js/main.js
@@ -16,53 +16,6 @@ var wow = new WOW(
   );
   wow.init();
 
-
-
-
-  /* View 
-    document.getElementById("line");
-      TweenLite.to(bar, 0.5, {width:"60px", height:"1px",  delay:1});
-  
-      document.getElementById("line1");
-      TweenLite.to(bar1, 1, {width:"1px", height:"175px",  delay:1});
-
-      document.getElementById("line2");
-      TweenLite.to(bar2, 0.5, {height: "80px", width:" 1px",  delay:0.5});
-  
-      document.getElementById("line3");
-      TweenLite.to(bar3, 1, {width:"65px", height:"1px",  delay:1});
-
-      document.getElementById("line4");
-      TweenLite.to(bar4, 1.5, {width:"1px", height:"70px",  delay:1.5});
-
-      document.getElementById("line5");
-      TweenLite.to(bar5, 0.5, {height: "170px", width: "1px", delay:0.5});
-  
-      document.getElementById("line6");
-      TweenLite.to(bar6, 1, {width:"15px", height:"1px",  delay:1});
-
-      document.getElementById("line7");
-      TweenLite.to(bar7, 1.5, {width:"1px", height:"35px",  delay:1.5});
-
-      document.getElementById("line8");
-      TweenLite.to(bar8, 1.5, {width:"1px", height:"58px",  delay:1.5});
-
-      document.getElementById("line9");
-      TweenLite.to(bar9, 1.5, {width:"1px", height:"40px",  delay:0.5});
-
-      document.getElementById("line10");
-      TweenLite.to(bar10, 1.5, {width:"50px", height:"1px",  delay:1});
-
-      document.getElementById("line11");
-      TweenLite.to(bar11, 1.5, {width:"1px", height:"15px",  delay:1.5});
-      */
-/*
-      $(function(){
-        setTimeout(function() {
-            $('.line-board').addClass("absolut")
-        }, 200);
-    });
-*/
 /* Методы для анимации при скролле */
 function isPartiallyVisible(el) {
   var elementBoundary = el.getBoundingClientRect();
@@ -70,7 +23,6 @@ function isPartiallyVisible(el) {
   var top = elementBoundary.top;
   var bottom = elementBoundary.bottom;
   var height = elementBoundary.height;
-  console.log(((top + height >= 0) && (height + window.innerHeight >= bottom)))
   return ((top + height >= 0) && (height + window.innerHeight >= bottom));
 }
 
@@ -86,16 +38,12 @@ function isFullyVisible(el) {
  function animationBox() {
   var bigPack = document.querySelector(".big-pack__content");
   var lines = bigPack.querySelectorAll("[data-scroll]");
-  console.log(lines);
-  console.log(bigPack);
   if(bigPack) {
       if (isPartiallyVisible(bigPack)) {
-        console.log("1")
           lines.forEach(line => {
             line.classList.add("absolut");
           });
       } else {
-        console.log("2")
         lines.forEach(line => {
           line.classList.remove("absolut");
         });
@@ -134,3 +82,6 @@ animationBox();
 }
 
 animationScroling();
+
+
+/* No scroll */
